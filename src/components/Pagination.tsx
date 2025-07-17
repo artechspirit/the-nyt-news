ae0f7f292ddeef7d35d6baa1d799331e6e4ffc96
artechspirit <artechspirit@gmail.com>
2025-07-18 00:15:53 +0700

    fix pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -5,27 +5,23 @@ interface PaginationProps {
   onPageChange: (page: number) => void;
 }
 
-export const Pagination = ({
-  currentPage,
-
-  onPageChange,
-}: PaginationProps) => {
+export const Pagination = ({ currentPage, onPageChange }: PaginationProps) => {
   return (
-    <div className="fixed flex justify-between gap-2 my-6 left-0 right-0 top-1/2 transform -translate-y-1/2 px-20">
+    <>
       <button
         disabled={currentPage === 0}
         onClick={() => onPageChange(currentPage - 1)}
-        className="px-3 py-1 disabled:opacity-50 cursor-pointer"
+        className="px-3 py-1 disabled:opacity-50 cursor-pointer fixed top-1/2 left-0 transform -translate-y-1/2"
       >
         <ChevronLeft size={100} />
       </button>
 
       <button
         onClick={() => onPageChange(currentPage + 1)}
-        className="px-3 py-1  disabled:opacity-50 cursor-pointer"
+        className="px-3 py-1  disabled:opacity-50 cursor-pointer  fixed top-1/2 right-0 transform -translate-y-1/2"
       >
         <ChevronRight size={100} />
       </button>
-    </div>
+    </>
   );
 };
